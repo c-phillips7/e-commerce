6a010834cefb1cd5a59beebd271e3757a4bcfa4d
Chris Phillips <cp.phillips15@gmail.com>
2022-01-09 20:18:13 -0500

    tested the poduct routes provided and added the delete route

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -127,6 +127,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(product => {
+    res.json(product)
+  })
+  .catch(err => {
+    console.log(err);
+  })
 });
 
 module.exports = router;
